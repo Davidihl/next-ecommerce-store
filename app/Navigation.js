c5520d69cf9e9d3f1f43adbb2749623d3f5d3c2e
David Ihl <daih1985@gmail.com>
2023-05-17 23:04:30 +0200

    Add logo and style header

--- app/Navigation.js
+++ app/Navigation.js
@@ -1,12 +1,16 @@
+import Image from 'next/image';
 import Link from 'next/link';
+import logo from '../public/Pulse_logo.svg';
 import styles from './Navigation.module.scss';
 
 export default function Navigation() {
   return (
     <nav className={styles.navigation}>
-      <Link href="/">PULSE</Link>
+      <Link href="/">
+        <Image src={logo} className={styles.logo} />
+      </Link>
       <div className={styles.menu}>
-        <Link href="/products">Products</Link>
+        <Link href="/products">Catalog</Link>
         <Link href="/cart">Cart</Link>
       </div>
     </nav>
