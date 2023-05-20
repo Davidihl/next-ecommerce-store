e6d56f2185237952bb9224c8fe57497071149601
David Ihl <daih1985@gmail.com>
2023-05-20 23:52:32 +0200

    Read out cart cookie on cart page

--- app/cart/Cart.js
+++ app/cart/Cart.js
@@ -1,5 +1,18 @@
+import { getProductById } from '../../database/products';
 import { checkCookie } from '../utility/checkCookie';
 import { getCookie } from '../utility/cookie';
+
+function renderCartItem(item) {
+  const productInCart = getProductById(item.id);
+  return (
+    <div
+      key={`cartItem-div-${productInCart.id}`}
+      data-test-id={`cart-product-${productInCart.id}`}
+    >
+      {productInCart.name}
+    </div>
+  );
+}
 
 export default function Cart() {
   const cartCookie = getCookie('yourCart'); // Get cookie from client as string
@@ -8,6 +21,7 @@ export default function Cart() {
   return (
     <main>
       <h1>Your Cart</h1>
+      {cart.map((cartItem) => renderCartItem(cartItem))}
     </main>
   );
 }
