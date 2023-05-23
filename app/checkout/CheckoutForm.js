87c5117f584abc82bbc4200c2475087cf07750af
David Ihl <daih1985@gmail.com>
2023-05-23 16:11:48 +0200

    Prepare checkout

--- app/checkout/CheckoutForm.js
+++ app/checkout/CheckoutForm.js
@@ -4,11 +4,11 @@ export default function CheckoutForm() {
   return (
     <form className={styles.form}>
       <label>
-        First name
+        First Name
         <input data-test-id="checkout-first-name" />
       </label>
       <label>
-        Last name
+        Last Name
         <input data-test-id="checkout-last-name" />
       </label>
       <label>
@@ -23,6 +23,29 @@ export default function CheckoutForm() {
         City:
         <input data-test-id="checkout-city" />
       </label>
+      <label>
+        Postal Code:
+        <input data-test-id="checkout-postal-code" />
+      </label>
+      <label>
+        Country:
+        <input data-test-id="checkout-country" />
+      </label>
+      <div className={styles.creditCard}>
+        <label>
+          Credit Cart Number:
+          <input data-test-id="checkout-credit-card" />
+        </label>
+        <label>
+          Expiration Date:
+          <input data-test-id="checkout-expiration-date" />
+        </label>
+        <label>
+          Security Code:
+          <input data-test-id="checkout-security-code" />
+        </label>
+      </div>
+      <button>Confirm</button>
     </form>
   );
 }
