58263f2bacf8e037a49762a3d59843af55c887c4
David Ihl <daih1985@gmail.com>
2023-05-26 15:56:22 +0200

    Style thank you page

--- app/checkout/thank-you/page.js
+++ app/checkout/thank-you/page.js
@@ -1,8 +1,22 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import thanks from '../../../public/images/thanks.png';
+import styles from './page.module.scss';
+
 export const metadata = {
   title: 'Animals page',
   description: 'My favorite animals',
 };
 
 export default function ThankYouPage() {
-  return <main>Thank you!</main>;
+  return (
+    <main className={styles.wrapper}>
+      <div>
+        <h1 className={styles.thankYou}>Thank you for your purchase!</h1>
+        <p>Your order has been confirmed</p>
+      </div>
+      <Image src={thanks} styles={styles.image} width={300} height={300} />
+      <Link href="/">Back to shopping</Link>
+    </main>
+  );
 }
