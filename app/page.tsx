e2d905917f77883109d4a8bdff1c4c1c1a54d8ad
David Ihl <daih1985@gmail.com>
2023-07-10 18:04:41 +0200

    Remove priority

--- app/page.tsx
+++ app/page.tsx
@@ -35,7 +35,6 @@ export default function Home() {
           src={heroImage}
           className={styles.heroImage}
           alt="Man wearing Pulse Headphones"
-          priority={true}
         />
       </section>
       <section className={styles.newArrival}>
